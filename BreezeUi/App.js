33e6d71475b218162753ebf987bf69f0b02ee9d8
19IM30010 <gyanendrajsp2001@gmail.com>
2024-01-19 10:23:49 +0530

    Just Playing

---
+++ BreezeUi/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import FlexDirectionBasics from './FlexDirectionBasics';
+// FlexDirectionBasics
+const App = () => {
+  return (
+    <View>
+      {/* <Text>Hello, React Native with JavaScript!</Text> */}
+      <FlexDirectionBasics/>
+      
+    </View>
+  );
+};
+
+export default App;
